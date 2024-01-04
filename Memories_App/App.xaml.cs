bf53f6992393d291d605d760773d63863e1c8dac
András Kozári <pordnb@INF.ELTE.HU>
2024-01-04 17:27:20 +0100

    Created prohject skeleton

---
+++ Memories_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Memories_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
