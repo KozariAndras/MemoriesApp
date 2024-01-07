a34e7b06c7f251aded20080121a30f179619eab3
András Kozári <pordnb@INF.ELTE.HU>
2024-01-07 02:31:25 +0100

    Fixed ambiguous namings, and now nothing works...

--- Memories_App/MainPage.xaml.cs
+++ Memories_App/MainPage.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace Memories_App
+﻿using Memories_App.Model;
+
+namespace Memories_App
 {
     public partial class MainPage : ContentPage
     {
 
+        private MemoriesAppModel _model;
+
         public MainPage()
         {
+            InitializeComponent();
+        }   
+
+        public MainPage(MemoriesAppModel model)
+        {
+            _model = model;
             InitializeComponent();
         }
 
--- Memories_App/UserMemoryView.xaml.cs
+++ Memories_App/UserMemoryView.xaml.cs
@@ -3,44 +3,44 @@ namespace Memories_App;
 public partial class UserMemoryView : ContentView
 {
 
-	public static readonly BindableProperty ImageProperty = BindableProperty.Create("Image", typeof(ImageSource), typeof(UserMemoryView));
-	public ImageSource Image
+	public static readonly BindableProperty ImageProperty = BindableProperty.Create(nameof(ImageValue), typeof(ImageSource), typeof(UserMemoryView));
+	public ImageSource ImageValue
 	{
         get => (ImageSource)GetValue(ImageProperty);
         set => SetValue(ImageProperty, value);
     }
 
-	public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(UserMemoryView));
-	public string Title
+	public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(TitleValue), typeof(string), typeof(UserMemoryView));
+	public string TitleValue
 	{
         get => (string)GetValue(TitleProperty);
         set => SetValue(TitleProperty, value);
     }
 
-	public static readonly BindableProperty DescriptionProperty = BindableProperty.Create("Description", typeof(string), typeof(UserMemoryView));
-	public string Description
+	public static readonly BindableProperty DescriptionProperty = BindableProperty.Create(nameof(DescriptionValue), typeof(string), typeof(UserMemoryView));
+	public string DescriptionValue
 	{
         get => (string)GetValue(DescriptionProperty);
         set => SetValue(DescriptionProperty, value);
     }
 
-	public static readonly BindableProperty DateProperty = BindableProperty.Create("Date", typeof(DateTime), typeof(UserMemoryView));
-	public DateTime Date
-	{
+	public static readonly BindableProperty DateProperty = BindableProperty.Create(nameof(DateValue), typeof(DateTime), typeof(UserMemoryView));
+	public DateTime DateValue
+    {
         get => (DateTime)GetValue(DateProperty);
         set => SetValue(DateProperty, value);
     }
 
-	public static readonly BindableProperty LocationProperty = BindableProperty.Create("Location", typeof(string), typeof(UserMemoryView));
-	public string Location
-	{
+	public static readonly BindableProperty LocationProperty = BindableProperty.Create(nameof(LocationValue), typeof(string), typeof(UserMemoryView));
+	public string LocationValue
+    {
         get => (string)GetValue(LocationProperty);
         set => SetValue(LocationProperty, value);
     }
 
-	public static readonly BindableProperty TagsProperty = BindableProperty.Create("Tags", typeof(List<string>), typeof(UserMemoryView));
-	public List<string> Tags
-	{
+	public static readonly BindableProperty TagsProperty = BindableProperty.Create(nameof(TagsValue), typeof(List<string>), typeof(UserMemoryView));
+	public List<string> TagsValue
+    {
         get => (List<string>)GetValue(TagsProperty);
         set => SetValue(TagsProperty, value);
     }
